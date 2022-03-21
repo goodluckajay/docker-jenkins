e60ed7a341d76371fd30e1b0a4188e73d068a31d
goodl <goodluckajay@gmail.com>
2022-03-21 16:47:17 +0530

    first commit

---
+++ src/main/java/com/self/DockerJenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsApplication.class, args);
+	}
+
+	@GetMapping("/jenkins")
+	public String dockerJenkinsIntegration(){
+		return "this is docker jenkins integration";
+	}
+
+}
