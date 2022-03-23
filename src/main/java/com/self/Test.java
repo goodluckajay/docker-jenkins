179d761f7e18c081220e9c8abcbf95ba2f84a2e0
goodl <goodluckajay@gmail.com>
2022-03-23 11:10:05 +0530

    123

--- src/main/java/com/self/Test.java
+++ src/main/java/com/self/Test.java
@@ -2,8 +2,8 @@ package com.self;
 
 public class Test {
     public void testMethod(){
-        System.out.println("test method");
-        System.out.println("test method1");
+        System.out.println("test method123");
+        System.out.println("test method123");
         System.out.println("23-03-2022");
     }
 }
